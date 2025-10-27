40da2fe986fb2310d9e8dacc31bbca5fe6854b9a
Javier <35864946+jmagit@users.noreply.github.com>
2025-10-27 10:40:37 +0100

    CommandLineRunner

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.err.println("Aplicación arrancada ...");
+	}
+	
+	@Bean
+	CommandLineRunner demos() {
+		return arg -> {
+			System.err.println("Desde el bean ...");
+		};
 	}
 
 }
