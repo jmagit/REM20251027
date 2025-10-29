7eb0a3d4d3d0abcadbe33b93ac148f5ff6674466
Javier <35864946+jmagit@users.noreply.github.com>
2025-10-29 12:43:05 +0100

    entorno

--- demo-data/src/main/java/com/example/DemoDataApplication.java
+++ demo-data/src/main/java/com/example/DemoDataApplication.java
@@ -1,13 +1,26 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class DemoDataApplication {
+public class DemoDataApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoDataApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.err.println("Aplicacion arrancada ...");
+	}
+
+	@Bean
+	CommandLineRunner demosDatos(EjemplosDatos demos) {
+		return args -> {
+			
+		};
+	}
 }
