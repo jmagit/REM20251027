6e4833c5e55236e463d11da6f1482478ea470b15
Javier <35864946+jmagit@users.noreply.github.com>
2025-10-31 09:01:16 +0100

    correo electronico

--- demo-data/src/main/java/com/example/DemoDataApplication.java
+++ demo-data/src/main/java/com/example/DemoDataApplication.java
@@ -1,9 +1,14 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.example.application.services.MessagingService;
+
+import jakarta.annotation.PreDestroy;
 
 @SpringBootApplication
 public class DemoDataApplication implements CommandLineRunner {
@@ -32,4 +37,35 @@ public class DemoDataApplication implements CommandLineRunner {
 			demos.categorias();
 		};
 	}
+
+//	@Autowired
+//	MessagingService mensajeria;
+//	
+//	@Bean
+//	CommandLineRunner demosCorreos() {
+//		return args -> {
+//			mensajeria.sendEmailAsync("pgrillo@example.com", "Aplicacion Init", "La aplicacion se ha iniciado");
+//			mensajeria.sendWelcomeEmailAsync("pgrillo@example.com", "Pepito Grillo");
+//		};
+//	}
+//	
+//	@PreDestroy
+//	void despidete() {
+//		var body = """
+//<!DOCTYPE html>
+//<html lang="es">
+//<head>
+//    <meta charset="UTF-8">
+//    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//    <title>Servicio</title>
+//</head>
+//<body>
+//    <h1>%s</h1>
+//    <p>%s</p>
+//</body>
+//</html>
+//""".formatted("Aplicacion Close", "La aplicacion se ha cerrado");
+//		mensajeria.sendMimeEmail("pgrillo@example.com", "Aplicacion Close", body, true);
+//	}
+
 }
